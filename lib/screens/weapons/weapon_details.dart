90c9b5c8d49afb7f4905553cb4fd9bcfe9c2d004
Pranjal Rane <rane.pranjal18@vit.edu>
2022-02-04 21:43:00 +0530

    bugs resolved

--- lib/screens/weapons/weapon_details.dart
+++ lib/screens/weapons/weapon_details.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:valorant_companion/models/weapons/weapon_data.dart';
